3d02538346d390c7b82ef722e49fae68dc715cd9
russell2440 <85845760+russell2440@users.noreply.github.com>
2021-08-03 15:03:26 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+class bst{
+  class node{
+
+  };
+
+public:
+  bst(){
+
+  }
+  void insert(int value){
+
+  }
+  void lookup(int value){
+
+  }
+  
+  node &traverse(node &n){
+
+  };
+};
+
+
+
+int main() {
+  printf("Hallow Russ! I love you!\n");
+  printf("Binary Search Tree unit test\n");
+  class bst tree;
+  tree.insert(9);
+  tree.insert(4);
+  tree.insert(6);
+  tree.insert(20);
+  tree.insert(170);
+  tree.insert(15);
+  tree.insert(1);
+  
+}
